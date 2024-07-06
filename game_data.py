105ec0b46de769f8484ca33bcb6e909149e208e1
Anish Shukla <138965632+shuklaji7058@users.noreply.github.com>
2024-07-06 19:33:12 +0530

    Create game_data.py

---
+++ game_data.py
@@ -0,0 +1,302 @@
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Rohit Sharma',
+        'follower_count': 216,
+        'description': 'Cricketer',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 259,
+        'description': 'Professional Cricketer',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Sonam Bajwa',
+        'follower_count': 174,
+        'description': 'Actress',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 172,
+        'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
+        'country': 'United States'
+    },
+    {
+        'name': 'Bharati Singh',
+        'follower_count': 167,
+        'description': 'Reality TV personality',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 149,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Hardik Pandya',
+        'follower_count': 145,
+        'description': 'Cricketer',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Neymar',
+        'follower_count': 138,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 135,
+        'description': 'Magazine',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 133,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Kumar Saanu ',
+        'follower_count': 131,
+        'description': 'Musician',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Kendall Jenner',
+        'follower_count': 127,
+        'description': 'Reality TV personality and Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Anish Shukla',
+        'follower_count': 333,
+        'description': 'Galli Cricketer, Coder',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Manoj Muntashir Shukla',
+        'follower_count': 113,
+        'description': 'Musician',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Nike',
+        'follower_count': 109,
+        'description': 'Sportswear multinational',
+        'country': 'United States'
+    },
+    {
+        'name': 'Suryakumar Yadav',
+        'follower_count': 108,
+        'description': 'Cricketer',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Narendra Modi',
+        'follower_count': 334,
+        'description': 'Prime  Minister Of Bharat',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Neha Kakkar',
+        'follower_count': 94,
+        'description': 'Musician',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Rupali Ganguly',
+        'follower_count': 90,
+        'description': 'Reality TV personality',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Kapil Sharma',
+        'follower_count': 89,
+        'description': 'Comedian and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Sunil Grover',
+        'follower_count': 87,
+        'description': 'Comedian and Actor',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Mumbai City FC',
+        'follower_count': 86,
+        'description': 'Football club',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Bengluru FC',
+        'follower_count': 85,
+        'description': 'Football club',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Mukesh Ambani',
+        'follower_count': 81,
+        'description': 'Businessman',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Demi Lovato',
+        'follower_count': 80,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': "Victoria's Secret",
+        'follower_count': 69,
+        'description': 'Lingerie brand',
+        'country': 'United States'
+    },
+    {
+        'name': 'Zendaya',
+        'follower_count': 68,
+        'description': 'Actress and musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Shakira',
+        'follower_count': 66,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Sunidhi Chahuan',
+        'follower_count': 65,
+        'description': 'Musician',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'B Praak',
+        'follower_count': 64,
+        'description': 'Musician',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'LeBron James',
+        'follower_count': 63,
+        'description': 'Basketball player',
+        'country': 'United States'
+    },
+    {
+        'name': 'Varun Dhawan',
+        'follower_count': 62,
+        'description': 'Actor',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Cardi B',
+        'follower_count': 67,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Sunil Chettri',
+        'follower_count': 82,
+        'description': 'Footballer',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Billie Eilish',
+        'follower_count': 61,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Timberlake',
+        'follower_count': 59,
+        'description': 'Musician and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'UEFA Champions League',
+        'follower_count': 58,
+        'description': 'Club football competition',
+        'country': 'Europe'
+    },
+    {
+        'name': 'Isro',
+        'follower_count': 56,
+        'description': 'Space agency',
+        'country': 'Bharat'
+    },
+    {
+        'name': 'Emma Watson',
+        'follower_count': 56,
+        'description': 'Actress',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Shawn Mendes',
+        'follower_count': 57,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Yuvraj Singh',
+        'follower_count': 55,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Gigi Hadid',
+        'follower_count': 54,
+        'description': 'Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Priyanka Chopra Jonas',
+        'follower_count': 53,
+        'description': 'Actress and musician',
+        'country': 'India'
+    },
+    {
+        'name': '9GAG',
+        'follower_count': 52,
+        'description': 'Social media platform',
+        'country': 'China'
+    },
+    {
+        'name': 'Ronaldinho',
+        'follower_count': 51,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'Maluma',
+        'follower_count': 50,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Camila Cabello',
+        'follower_count': 49,
+        'description': 'Musician',
+        'country': 'Cuba'
+    },
+    {
+        'name': 'NBA',
+        'follower_count': 47,
+        'description': 'Club Basketball Competition',
+        'country': 'United States'
+    }
+]
